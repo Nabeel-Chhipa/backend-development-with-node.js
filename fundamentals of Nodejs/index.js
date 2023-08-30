11317a9f6039302d80078e7f60adcc45be23895d
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-08-30 23:24:45 +0500

    node.js file system intro

--- fundamentals of Nodejs/index.js
+++ fundamentals of Nodejs/index.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+const app = require('./app')
+const fs = require('fs')
+
+// var x = '20'
+// if(x === 20) console.log('yes')
+// else console.log('no')
+
+// for(let i = 0; i < 10; i++) console.log(i)
+
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let result = data.filter(item => item <= 5)
+// console.log(app)
+
+// fs.writeFileSync('demo.txt', `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`)
+
+console.log('Directory name: ', __dirname)
+console.log('File name: ', __filename)
