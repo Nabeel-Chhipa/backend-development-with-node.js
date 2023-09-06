9a5227e7e8145a9a240bde3a54f95c286472946a
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-09-07 00:27:10 +0500

    create simple api with dummy data

---
+++ simple API/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    name: "Muhammad Nabeel",
+    country: "Pakistan",
+    city: "Karrachi",
+  },
+  {
+    name: "Muhammad Usama",
+    country: "Pakistan",
+    city: "Lahore",
+  },
+  {
+    name: "Muhammad Saad",
+    country: "Pakistan",
+    city: "Islamabad",
+  },
+];
+module.exports = data;
